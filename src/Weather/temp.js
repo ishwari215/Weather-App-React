abcabdfc4869d078a96c32f00266662b9fa22068
Ishwari Niphade <ishwari2152@gmail.com>
2021-10-22 10:38:39 +0530

    My first React App : Weather App

---
+++ src/Weather/temp.js
@@ -0,0 +1,73 @@
+// api.openweathermap.org/data/2.5/weather?q=pune&appid=22ab33e76d688471f73bddb208ab689b
+
+import React, { useState, useEffect } from "react";
+import WeatherCard from "./weatherCard";
+import "./style.css";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("pune");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=22ab33e76d688471f73bddb208ab689b`;
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Enter City"
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card */}
+      <WeatherCard tempInfo={tempInfo}/>
+    </>
+  );
+};
+
+export default Temp;
